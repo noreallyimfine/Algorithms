25c5c83bbff25b6cf377cb4c11ff6f0a5527f766
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-01-22 18:20:51 -0500

    pass small eating_cookies tests, edited out lists from large tests cuz im using cache dict

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -7,21 +7,24 @@ import sys
 # a solution that is more efficient than the naive
 # recursive solution
 def eating_cookies(n, cache=None):
+    if not cache:
+        cache = {1: 1, 2: 2}
     # base case - 0 cookies
+    print(cache)
     if n < 2:
-        return 1
+        return cache[1]
     # base case - 1 cookie
     elif n == 2:
-        return 2
-    elif n == 3:
-        return 4
+        return cache[2]
     else:
-        return eating_cookies(n-1) + eating_cookies(n-2)
+        cache[n] = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+        return cache[n]
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         num_cookies = int(sys.argv[1])
-        print("There are {ways} ways for Cookie Monster to eat {n} cookies.".format(ways=eating_cookies(num_cookies), n=num_cookies))
+        print("There are {ways} ways for Cookie Monster to eat {n} cookies."
+              .format(ways=eating_cookies(num_cookies), n=num_cookies))
     else:
         print('Usage: eating_cookies.py [num_cookies]')
